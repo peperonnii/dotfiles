1a109681d65599ea69f6a9b2763c4370319e18a6
BigLadDylan <BruceWayne0002@proton.me>
2024-03-21 00:03:49 +0000

    03-21-2024

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,16 +1,11 @@
 #!/bin/zsh
-
 mkdir -p ~/dotfiles
 
-# Copy .config directory, .p10k.zsh, and .zshrc to ~/dotfiles
-# Note: The -r flag is for recursive copying (for directories), and -rL follows symbolic links
 cp -rL ~/.config/hypr ~/dotfiles/
 cp -rL ~/.config/nvim ~/dotfiles/
 cp -rL ~/.config/alacritty ~/dotfiles/
-cp -rL ~/.config/astro ~/dotfiles/
 cp -rL ~/.config/awesome ~/dotfiles/
 cp -rL ~/.config/cava ~/dotfiles/
-cp -rL ~/.config/discord ~/dotfiles/
 cp -rL ~/.config/kitty ~/dotfiles/
 cp -rL ~/.config/mpv ~/dotfiles/
 cp -rL ~/.config/neofetch ~/dotfiles/
@@ -24,9 +19,6 @@ cp -rL ~/.config/yazi ~/dotfiles/
 cp -rL ~/.p10k.zsh ~/dotfiles/
 cp -rL ~/.zshrc ~/dotfiles/
 
-# Export the list of explicitly installed packages to packages.txt
-# Note: This command requires sudo, so either run the script as a superuser or modify permissions as needed
-sudo pacman -Qqe > ~/dotfiles/packages.txt
-
+sudo pacman -Qqe | tr '\n' ' ' > ~/dotfiles/packages.txt
 echo "Backup completed."
 
